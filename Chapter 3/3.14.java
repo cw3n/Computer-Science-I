4e6331ddcde9a9710a1933f782e0a010495fd46b
cw3n <71278604+cw3n@users.noreply.github.com>
2023-02-01 11:27:39 -0600

    Add files via upload

--- Chapter 3/3.14.java
+++ Chapter 3/3.14.java
@@ -1,27 +1,17 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2708
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 ArialMT;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl337\partightenfactor0
-
-\f0\fs29\fsmilli14667 \cf2 \expnd0\expndtw0\kerning0
-import java.util.Scanner;\
-\'a0\
-public class Exercise_03_14 \{\
-\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 public static void main(String[] args) \{\
-\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 Scanner input = new Scanner(System.in);\
-\'a0\
-\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 // Generate a random integer 0 or 1\
-\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 int coin = (int)(Math.random() * 2);\
-\'a0\
-\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 // Prompt the user to enter a guess\
-\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 System.out.print("Enter a guess 0-head or 1-tail: ");\
-\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 int guess = input.nextInt();\
-\'a0\
-\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 // Display result\
-\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 System.out.println(((guess == coin) ? "Correct" : "Incorrect") + " guess."); \
-\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 \}\
-\}\
+import java.util.Scanner;
+ 
+public class Exercise_03_14 {
+            public static void main(String[] args) {
+                        Scanner input = new Scanner(System.in);
+ 
+                        // Generate a random integer 0 or 1
+                        int coin = (int)(Math.random() * 2);
+ 
+                        // Prompt the user to enter a guess
+                        System.out.print("Enter a guess 0-head or 1-tail: ");
+                        int guess = input.nextInt();
+ 
+                        // Display result
+                        System.out.println(((guess == coin) ? "Correct" : "Incorrect") + " guess."); 
+            }
 }
